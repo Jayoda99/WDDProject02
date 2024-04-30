28e583365e09e7192e433dba8d3f4e18ffce2463
Jayoda99 <138502443+Jayoda99@users.noreply.github.com>
2024-04-30 18:02:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+$(document) .ready(function function_name() {
+	$(window).scroll(function(){
+		if(this.scrollY > 20){
+			$('.navbar').addClass("sticky");
+		}
+		else{
+			$('.navbar').removeClass("sticky");
+		}
+	});
 
+	//Navigation clisk function
+
+	$('.menu-btn').click(function(){
+		$('.navbar .menu').toggleClass(active)
+		$('.menu-btn i').toggleClass(active)
+	}); 
+});
